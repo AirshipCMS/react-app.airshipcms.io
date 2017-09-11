da0ecd32acc0c8dd91e58a78c963348933634b44
Te Vallee <astronomemusic@gmail.com>
2017-09-11 10:32:07 -1000

    added class .github-link

--- src/components/Github/index.js
+++ src/components/Github/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Github(props) {
   return (
     <a
-      className="nav-item is-tab github"
+      className="nav-item is-tab github-link"
       href="https://github.com/AirshipCMS/react-app.airshipcms.io"
     >
       <img src="/assets/media/github-icon.svg" alt="github"/>
